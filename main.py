65b6d757fe751d383765223ff5a7152961d81c81
Shantanu Rahman <rahmanshantanu69@gmail.com>
2019-11-01 01:00:49 +0600

    Api taking shape

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, escape
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
   return render_template('index.html')
+
+@app.route('/api/<name>/<target>')
+def result(name, target):
+  name = escape(name)
+  target = escape(target)
+  return 'Calling {} with {}'.format(name, target)
 
 if __name__ == '__main__':
   app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,8 +1,27 @@
+function get_content(name, inst, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(xhr.responseText);
+    }
+  }
+
+  xhr.open('GET', '/api/' + name + '/' + inst);
+  xhr.send();
+}
+
 function main() {
-  var btn = document.getElementById('submit_btn');
-  btn.addEventListener('click', function(e) {
+  var btn = document.getElementById('submit_btn')
+  .addEventListener('click', function(e) {
     e.preventDefault();
-    console.log('Clicked!');
+    var name = document.getElementById('name_input')
+                .value.replace(/\s/gi, '_');
+    var menu = document.getElementById('tgt_inst_input');
+    var inst = menu.options[menu.selectedIndex].text.toLowerCase();
+
+    get_content(name, inst, function(data) {
+      console.log(data);
+    });
   });
 }
 
